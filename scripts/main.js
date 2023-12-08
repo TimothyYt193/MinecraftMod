3d602092f6e5ebb1e3d8f5f96b3743a7f4cd44ce
T1M0THYYt <84844650+TimothyYt193@users.noreply.github.com>
2023-12-08 13:09:55 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
-console.log('Hello World!');
-
 function openWebsite(url) {
   window.open(url, "_blank");
 }
+
+function onClick() {
+  var audio = new Audio('sounds/button_click.ogg');
+  audio.play();
+}
+
+document.addEventListener('mousemove', (e) => {
+  const cursor = document.querySelector('.custom-cursor');
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
